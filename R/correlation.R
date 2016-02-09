f40cf96c54678cf22892ba5759eb6969d3653e02
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2016-02-09 12:45:39 +0100

    Added ordering functions and relative tests

--- R/correlation.R
+++ R/correlation.R
@@ -25,3 +25,59 @@ min.fData = function( fData, ..., which = FALSE )
   }
 
 }
+
+max_ordered = function( fData, gData )
+{
+  if( fData$P != gData$P ||
+      fData$h != gData$h ||
+      fData$t0 != gData$t0 ||
+      fData$tP != gData$tP  )
+  {
+    stop( ' Error in max_ordered: provided fData objects have mismatching time grids')
+  }
+
+  if( fData$N != gData$N )
+  {
+    fData$N == 1 || gData$N == 1 ||
+      stop( ' Error  in max_ordered: you must provide equally sized fData
+            objects or at least one of them with 1 observation.')
+  }
+
+  return( max( fData ) - max( gData ) <= 0 )
+}
+
+
+area_under_curve = function( fData)
+{
+  return( rowSums( ( fData$values[ , - 1 ] +
+                       fData$values[ , - fData$P ] ) / 2 * fData$h ) )
+}
+
+area_ordered = function( fData, gData )
+{
+  if( fData$P != gData$P ||
+      fData$h != gData$h ||
+      fData$t0 != gData$t0 ||
+      fData$tP != gData$tP  )
+  {
+    stop( ' Error in area_ordered: provided fData objects have mismatching time grids')
+  }
+
+  if( fData$N != gData$N )
+  {
+    if( fData$N == 1 )
+    {
+      return( area_under_curve( gData - fData$values ) >= 0 )
+    } else if( gData$N == 1 )
+    {
+      return( area_under_curve( fData - gData$values ) <= 0 )
+    } else {
+      stop( ' Error  in area_ordered: you must provide equally sized fData
+            objects or at least one of them with 1 observation.')
+    }
+  } else {
+    return( area_under_curve( fData - gData ) <= 0 )
+  }
+}
+
+
--- tests/testthat/test_correlation.R
+++ tests/testthat/test_correlation.R
@@ -1,7 +1,7 @@
 
 # TESTING MAX-MIN FUNCTIONS -----------------------------------------------
 
-P = 1e3
+P = 1e4
 
 time_grid = seq( 0, 1, length.out = P )
 
@@ -19,11 +19,11 @@ fD = fData( time_grid, Data )
 
 test_that( 'Max function for functional data, which = TRUE, grid',
            expect_equal( max( fD, which = TRUE )$grid,
-                             c( 1, 1, 0 + 499 * h ) ) )
+                             c( 1, 1, 0 + 4999 * h ) ) )
 
 test_that( 'Max function for functional data, which = TRUE, value',
            expect_equal( max( fD, which = TRUE )$value,
-                             c( 1, 2, 3 * ( 0.5 - abs( 0.5 - 499 * h) ) ) ) )
+                             c( 1, 2, 3 * ( 0.5 - abs( 0.5 - 4999 * h) ) ) ) )
 
 test_that( 'Min function for functional data, which = TRUE, grid',
            expect_equal( min( fD, which = TRUE )$grid,
@@ -35,8 +35,81 @@ test_that( 'Min function for functional data, which = TRUE, value',
 
 test_that( 'Max function for functional data, which = FALSE',
            expect_equal( max( fD, which = FALSE ),
-                         c( 1, 2, 3 * ( 0.5 - abs( 0.5 - 499 * h) ) ) ) )
+                         c( 1, 2, 3 * ( 0.5 - abs( 0.5 - 4999 * h) ) ) ) )
 
 test_that( 'Min function for functional data, which = FALSE',
            expect_equal( min( fD, which = FALSE ),
                          c( 0, 0, 0 ) ) )
+
+test_that( 'Area under the curve',
+           expect_equal( area_under_curve( fD ),
+                         c( 0.5, 1, 0.75 ) ) )
+
+# ORDERING ----------------------------------------------------------------
+
+P = 1e3
+
+time_grid = seq( 0, 1, length.out = P )
+
+h = time_grid[ 2 ] - time_grid[ 1 ]
+
+Data_1 = matrix( c( 1 * time_grid,
+                    2 *  time_grid ),
+                 nrow = 2, ncol = P, byrow = TRUE )
+
+Data_2 = matrix( 3 * ( 0.5 - abs( time_grid - 0.5 ) ),
+                 nrow = 1, byrow = TRUE )
+
+Data_3 = rbind( Data_1, Data_1 )
+
+
+fD_1 = fData( time_grid, Data_1 )
+fD_2 = fData( time_grid, Data_2 )
+fD_3 = fData( time_grid, Data_3 )
+
+# MAX ORDERING
+test_that( 'Max_ordering - case 1',
+           expect_equal( max_ordered( fD_1, fD_2 ),
+                         c( TRUE, FALSE ) ) )
+
+test_that( 'Max_ordering - case 2',
+           expect_equal( max_ordered( fD_2, fD_1 ),
+                         c( FALSE, TRUE ) ) )
+
+test_that( 'Max_ordering - case 3',
+           expect_error( max_ordered( fD_1, fD_3 ) ) )
+
+test_that( 'Max_ordering - case 4',
+           expect_error( max_ordered( fD_3, fD_1 ) ) )
+
+test_that( 'Max_ordering - case 5',
+           expect_equal( max_ordered( fD_2, fD_3 ),
+                         c( FALSE, TRUE, FALSE, TRUE ) ) )
+
+test_that( 'Max_ordering - case 6',
+           expect_equal( max_ordered( fD_3, fD_2 ),
+                         c( TRUE, FALSE, TRUE, FALSE ) ) )
+
+# AREA ORDERING
+test_that( 'Area ordering - case 1',
+           expect_equal( area_ordered( fD_1, fD_2 ),
+                         c( TRUE, FALSE ) ) )
+
+test_that( 'Area ordering - case 2',
+           expect_equal( area_ordered( fD_2, fD_1 ),
+                         c( FALSE, TRUE ) ) )
+
+test_that( 'Max_ordering - case 3',
+           expect_error( area_ordered( fD_1, fD_3 ) ) )
+
+test_that( 'Max_ordering - case 4',
+           expect_error( area_ordered( fD_3, fD_1 ) ) )
+
+test_that( 'Max_ordering - case 5',
+           expect_equal( area_ordered( fD_2, fD_3 ),
+                         c( FALSE, TRUE, FALSE, TRUE ) ) )
+
+test_that( 'Max_ordering - case 6',
+           expect_equal( max_ordered( fD_3, fD_2 ),
+                         c( TRUE, FALSE, TRUE, FALSE ) ) )
+
