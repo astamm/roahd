262b1f3431da826beb062ae87b677acfffe2b7d1
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2016-03-02 18:35:17 +0100

    Updated vignettes, fbplot and DESCRIPTION

--- R/fbplot.R
+++ R/fbplot.R
@@ -48,7 +48,7 @@
 #'  to the averaged adjusted value \eqn{\bar{F}}. Default is 20;}
 #'  \item{"\code{trial_size}"}{: the number of elements in the gaussian
 #'  population of functional data that will be simulated at each repetition of
-#'  the adjustment procedure. Default is \code{5 * Data$N};}
+#'  the adjustment procedure. Default is \code{Data$N};}
 #'  \item{"\code{TPR}"}{: the True Positive Rate of outliers, i.e. the proportion
 #'  of observations in a dataset without amplitude outliers that have to be
 #'  considered outliers. Default is \code{2 * pnorm( 4 * qnorm( 0.25 ) )};}
@@ -235,7 +235,7 @@ fbplot.fData = function( Data,
                        adjust$N_trials )
 
     trial_size = ifelse( is.null( adjust$trial_size ),
-                         5 * Data$N,
+                         Data$N,
                          adjust$trial_size )
 
     TPR = ifelse( is.null( adjust$TPR ),
