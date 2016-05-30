e036a0d86eee3ec37b88ef0e06fb3fb48a986702
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2016-05-30 15:06:57 +0200

    Removed uniformity check in fData

--- R/fData.R
+++ R/fData.R
@@ -59,12 +59,6 @@
 #'
 fData = function( grid, values )
 {
-  if( length( unique( diff( grid ) ) ) > 1 )
-  {
-    max( diff( unique( diff( grid ) ) ) ) / diff( range( grid ) ) < 1e-10  ||
-      stop( ' Error in fData: you provided an unevenly spaced grid')
-  }
-
   P = length( grid )
 
   values = toRowMatrixForm( values )
