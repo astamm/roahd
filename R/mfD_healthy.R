a70cbcb2ea303572d437aa42dabdc0a40df6c9e6
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2018-04-07 20:42:57 +0200

    Added ECG data to package

---
+++ R/mfD_healthy.R
@@ -0,0 +1,9 @@
+#' ECG trace of healthy subjects
+#'
+#' A dataset containing the 8-Lead ECG traces of 150 healthy subjects. They can be used to compare the
+#' signals of pathological subjects stored in \code{mfD_LBBB} and \code{mfD_RBBB} objects.
+#'
+#' The 8 leads are, in order, V1, V2, V3, V4, V5, D1 and D2. The signals have been registered and
+#' smoothed over an evenly spaced grid of 1024 time points at 1kHz.
+#'
+"mfD_healthy"
