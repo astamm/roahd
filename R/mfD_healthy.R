12d5b0812b03c2c5f40ffbc059ea5134cfde9e39
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2018-05-02 10:06:32 +0200

    Reduced dataset of healthy subjects (due to CRAN restriction on pkg size)

--- R/mfD_healthy.R
+++ R/mfD_healthy.R
@@ -1,6 +1,6 @@
 #' ECG trace of healthy subjects
 #'
-#' A dataset containing the 8-Lead ECG traces of 150 healthy subjects. They can be used to compare the
+#' A dataset containing the 8-Lead ECG traces of 50 healthy subjects. They can be used to compare the
 #' signals of pathological subjects stored in \code{mfD_LBBB} and \code{mfD_RBBB} objects.
 #'
 #' The 8 leads are, in order, V1, V2, V3, V4, V5, D1 and D2. The signals have been registered and
